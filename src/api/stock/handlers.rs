13c3e4a2b7440cde11129e06319c407dfdca3460
Paul-Sebastian Manole <brokenthorn@gmail.com>
2020-08-04 17:30:31 +0300

    Added an important TODO comment

--- src/api/stock/handlers.rs
+++ src/api/stock/handlers.rs
@@ -4,6 +4,7 @@ use tide::{Body, Request, Response, Result, StatusCode};
 
 /// Handler function for __GET__ `/stock`
 pub async fn get_stock(req: Request<crate::api::context::Context>) -> Result<Response> {
+    // TODO: Pass data_curenta and data_ultima_actualizare from request parameters or the current defaults.
     let now_datetime = chrono::Local::now().format("%Y%m%d %H:%M:%S").to_string();
     let data_curenta = now_datetime.as_ref();
     let data_ultima_actualizare = "19000101";
